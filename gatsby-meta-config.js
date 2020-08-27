8f92da08b82f41da5c4d8e33db0428df6cb547d7
wjy5446 <wjy5446@gmail.com>
2020-08-27 21:49:44 +0900

    feat) modify meta-config

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -1,30 +1,30 @@
 module.exports = {
-  title: `Bee starter`,
-  description: `Blog posted about ...`,
-  author: `[Your Name]`,
-  introduction: `I explain with words and code. I explain with words and code. I explain with words and code.`,
-  siteUrl: `https://gatsby-starter-bee.netlify.com`, // Your blog site url
-  social: {
-    twitter: ``, // Your Twitter account
-    github: ``, // Your GitHub account
-    medium: ``, // Your Medium account
-    facebook: ``, // Your Facebook account
-    linkedin: ``, // Your LinkedIn account
-  },
-  icon: `content/assets/felog.png`, // Add your favicon
-  keywords: [`blog`],
-  comment: {
-    disqusShortName: '', // Your disqus-short-name. check disqus.com.
-    utterances: 'JaeYeopHan/gatsby-starter-bee', // Your repository for archive comment
-  },
-  configs: {
-    countOfInitialPost: 10, // Config your initial count of post
-  },
-  sponsor: {
-    buyMeACoffeeId: 'jbee',
-  },
-  share: {
-    facebookAppId: '', // Add facebookAppId for using facebook share feature v3.2
-  },
-  ga: '', // Add your google analytics tranking ID
+    title: `Whale's Blog`,
+    description: `Somethong for your brain`,
+    author: `WHALE`,
+    introduction: `Fun Coding`,
+    siteUrl: `https://whlae-blog.netlify.com`, // Your blog site url
+    social: {
+        twitter: ``, // Your Twitter account
+        github: `wjy5446`, // Your GitHub account
+        medium: ``, // Your Medium account
+        facebook: `jaeyoung.yoon.940`, // Your Facebook account
+        linkedin: `jaeyoung-yoon`, // Your LinkedIn account
+    },
+    icon: `content/assets/felog.png`, // Add your favicon
+    keywords: [`blog`],
+    comment: {
+        disqusShortName: 'wjy5446', // Your disqus-short-name. check disqus.com.
+        utterances: 'wjy5446/blog', // Your repository for archive comment
+    },
+    configs: {
+        countOfInitialPost: 10, // Config your initial count of post
+    },
+    sponsor: {
+        buyMeACoffeeId: 'jbee',
+    },
+    share: {
+        facebookAppId: '', // Add facebookAppId for using facebook share feature v3.2
+    },
+    ga: '', // Add your google analytics tranking ID
 }
